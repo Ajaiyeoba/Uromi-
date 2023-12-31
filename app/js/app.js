3345bae381244e17291dbd57d8133df8e40e7871
Ajaiyeoba <ajaiyeobajibola@gmail.com>
2023-12-31 09:23:15 -0800

    Presently working on the mobile view navlist and animated hambugger

--- app/js/app.js
+++ app/js/app.js
@@ -1,1 +1,11 @@
-console.log('hello world!')
+const hamBtn = document.querySelector('#hamBtn');
+const header = document.querySelector('.header')
+hamBtn.addEventListener('click',function() { 
+    console.log('clicked Hamburger');
+
+    if (header.classList.contains('open')) {
+        header.classList.remove('open');
+    } else {
+    header.classList.add('open');
+    }
+});
