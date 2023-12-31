b57a455273ace8e4dfce420962baa3f2554a18eb
Ajaiyeoba <ajaiyeobajibola@gmail.com>
2023-12-31 10:06:10 -0800

    another commit

--- app/js/app.js
+++ app/js/app.js
@@ -1,11 +1,20 @@
 const hamBtn = document.querySelector('#hamBtn');
 const header = document.querySelector('.header')
+const overlay = document.querySelector('.overlay');
+
+
+
 hamBtn.addEventListener('click',function() { 
     console.log('clicked Hamburger');
 
     if (header.classList.contains('open')) {
-        header.classList.remove('open');
+        header.classList.remove('open'); // Closes Hamburger
+        overlay.classList.add('.fade-out');
+        overlay.classList.remove('.fade-in');
     } else {
-    header.classList.add('open');
+    header.classList.add('open'); // opens hamburger
+    overlay.classList.add('.fade-in');
+    overlay.classList.remove('.fade-out');
+
     }
 });
