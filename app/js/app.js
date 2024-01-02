73742d4aee28240e6b6217ea7d3330db91d5aba7
Ajaiyeoba <ajaiyeobajibola@gmail.com>
2024-01-02 09:06:38 -0800

    finally done with the nav! Now starting work on the hero section.

--- app/js/app.js
+++ app/js/app.js
@@ -1,4 +1,5 @@
 const hamBtn = document.querySelector('#hamBtn');
+const body = document.querySelector('body');
 const header = document.querySelector('.header')
 const overlay = document.querySelector('.overlay');
 const fadeElem = document.querySelectorAll('.has-fade');
@@ -10,6 +11,7 @@ hamBtn.addEventListener('click',function() {
 
     if (header.classList.contains('open')) {
         header.classList.remove('open'); // Closes Hamburger
+        body.classList.remove('noscroll');
         fadeElem.forEach(function( element){
         element.classList.remove('fade-in');
         element.classList.add('fade-out');
@@ -17,6 +19,7 @@ hamBtn.addEventListener('click',function() {
 
     }
      else{
+    body.classList.add('noscroll');
     header.classList.add('open'); // opens hamburger
     fadeElem.forEach(function(element) {
     element.classList.remove('fade-out');
